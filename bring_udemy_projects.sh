9726c29027beaf1e500ea911bdb6db54b14260da
Luis Roberto <luisrobertopenav@gmail.com>
2022-09-06 11:07:03 -0700

    Add note from 29-08 and add photo-app project advancement

--- bring_udemy_projects.sh
+++ bring_udemy_projects.sh
@@ -3,7 +3,8 @@
 projects=()
 # projects+=("/Users/luisroberto/Documents/ENROUTE/RoR-Workshop/udemy-ror-projects/alpha-blog")
 # projects+=("/Users/luisroberto/Documents/ENROUTE/RoR-Workshop/udemy-ror-projects/message_me")
-projects+=("/Users/luisroberto/Documents/ENROUTE/RoR-Workshop/udemy-ror-projects/finance-tracker")
+# projects+=("/Users/luisroberto/Documents/ENROUTE/RoR-Workshop/udemy-ror-projects/finance-tracker")
+projects+=("/Users/luisroberto/Documents/ENROUTE/RoR-Workshop/udemy-ror-projects/photo-app")
 
 for project in "${projects[@]}" ;
 do
---
+++ udemy/projects/photo-app/README.md
@@ -0,0 +1,11 @@
+# README
+Web application using rails. 
+Project made following a udemy RoR course. 
+
+<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ruby_logo.svg/400px-Ruby_logo.svg.png" width="250px" alt="Ruby icon">
+
+## About the project
+Application to upload photos and share them to the world. 
+
+## Udemy original project link
+[Link to original udemy project](https://github.com/udemyrailscourse/photo-app)
