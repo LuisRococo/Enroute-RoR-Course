ea6513059665251abe9178b54110e9a82dce1f10
Luis Roberto <luisrobertopenav@gmail.com>
2022-09-12 18:03:26 -0700

    Add complete 'univ-app' project

--- bring_udemy_projects.sh
+++ bring_udemy_projects.sh
@@ -4,7 +4,9 @@ projects=()
 # projects+=("/Users/luisroberto/Documents/ENROUTE/RoR-Workshop/udemy-ror-projects/alpha-blog")
 # projects+=("/Users/luisroberto/Documents/ENROUTE/RoR-Workshop/udemy-ror-projects/message_me")
 # projects+=("/Users/luisroberto/Documents/ENROUTE/RoR-Workshop/udemy-ror-projects/finance-tracker")
-projects+=("/Users/luisroberto/Documents/ENROUTE/RoR-Workshop/udemy-ror-projects/photo-app")
+# projects+=("/Users/luisroberto/Documents/ENROUTE/RoR-Workshop/udemy-ror-projects/photo-app")
+projects+=("/Users/luisroberto/Documents/ENROUTE/RoR-Workshop/udemy-ror-projects/univ-app")
+
 
 for project in "${projects[@]}" ;
 do
